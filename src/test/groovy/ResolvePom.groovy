dac24b03d7b0dffa5a417512efffe9b169100cee
per <per@alipsa.se>
2025-04-17 22:20:43 +0200

    release of 3.9.4 v1.1.0

---
+++ src/test/groovy/ResolvePom.groovy
@@ -0,0 +1,58 @@
+@Grapes([
+        @Grab(group='org.apache.maven', module='maven-core', version='3.0.5'),
+        @Grab(group='org.apache.maven', module='maven-compat', version='3.0.5'),
+        @Grab(group='com.jcabi', module='jcabi-aether', version='0.10.1')
+])
+
+// http://www.programcreek.com/java-api-examples/index.php?api=org.apache.maven.project.MavenProjectBuilder See # 20
+
+
+import org.codehaus.plexus.DefaultPlexusContainer
+import org.apache.maven.project.MavenProjectBuilder
+import org.apache.maven.artifact.repository.layout.ArtifactRepositoryLayout;
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.project.DefaultProjectBuilderConfiguration
+import org.apache.maven.artifact.repository.DefaultArtifactRepository
+import com.jcabi.aether.Aether
+import org.sonatype.aether.repository.RemoteRepository;
+import org.sonatype.aether.util.artifact.DefaultArtifact;
+import org.sonatype.aether.artifact.Artifact
+
+import java.nio.file.Paths;
+
+
+container=new DefaultPlexusContainer();
+projectBuilder=(MavenProjectBuilder)container.lookup(MavenProjectBuilder.class.getName());
+layout=(ArtifactRepositoryLayout)container.lookup(ArtifactRepositoryLayout.class.getName(),"default");
+
+def projectInfo(String localRepoUrl, File pomFile){
+    String localRepoUrl2 = "file://" + localRepoUrl;
+    File local = new File(localRepoUrl);
+
+    ArtifactRepository localRepo = new DefaultArtifactRepository("local",localRepoUrl2,layout);
+    pbConfig = new DefaultProjectBuilderConfiguration().setLocalRepository(localRepo);
+    project = projectBuilder.build( pomFile, pbConfig );
+    aether = new Aether(project, local);
+    [ runtime: resolveDependencies(aether, project, "runtime"),
+      test : resolveDependencies(aether, project, "test") ];
+}
+
+
+def resolveDependencies (aether, project, scope) {
+    depLists = project.getDependencies().collect {
+
+        art = new DefaultArtifact(it.getGroupId(), it.getArtifactId(), it.getClassifier(), it.getType(),
+                it.getVersion());
+        Collection<Artifact> deps = aether.resolve( art, scope );
+
+        deps.collect{  it.getFile().getAbsolutePath() }
+
+    }
+
+    [ dependencies : depLists.collect {it.first()},  classpath : depLists.flatten() ]
+}
+
+
+//File pomFile = Paths.get(this.getClass().getResource("/pom/transientPom.xml").toURI()).toFile();
+File pomFile = new File(inout.projectDir(), "src/test/resources/pom/transientPom.xml")
+println projectInfo("/home/per/.m2/repository", pomFile);
